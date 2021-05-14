803e8e6b38f640085d06935ec9d880e34d5d596f
Kunal Soni <53283003+kunal9922@users.noreply.github.com>
2021-05-14 14:30:10 +0530

    added feature to convert raw string into char array

--- AES/cyperAES.java
+++ AES/cyperAES.java
@@ -1,7 +1,30 @@
 import java.util.Scanner;
+
 public class cyperAES {
-    
-    Scanner inp = new Scanner(System.in);
-    System.out.println()
-    String plainText = inp.next();
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the text to securly Encrypt");
+        String plainText = scan.next();
+
+        // create an array of size 32 byte to store each character from given sting and
+        // do encryption
+        char[][] plainTxtGrid = new char[8][8];
+        int charAtCount = 0;
+
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
+                if (charAtCount < plainText.length()) {
+                    plainTxtGrid[r][c] = plainText.charAt(charAtCount);
+                    charAtCount++;
+                } else
+                    plainTxtGrid[r][c] = 'Z'; // Input string is end then add dummy char
+            }
+        }
+        for (int r = 0; r < 8; r++) {
+            for (int c = 0; c < 8; c++) {
+                System.out.print(plainTxtGrid[r][c] + " ");
+            }
+        }
+
+    }
 }
