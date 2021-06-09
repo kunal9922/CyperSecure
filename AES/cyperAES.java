deabc0d874c2754fa4874fc38e5621a7474fec06
Kunal Soni <53283003+kunal9922@users.noreply.github.com>
2021-06-09 14:01:35 +0530

    Added function g() for a word

--- AES/cyperAES.java
+++ AES/cyperAES.java
@@ -1,39 +1,120 @@
 import java.util.Scanner;
 
 class AES {
+    private int round = 1;
+    final private String[][] sBox = new String[][] {
+            { "63", "7c", "77", "7b", "f2", "6b", "6f", "c5", "30", "01", "67", "2b", "fe", "d7", "ab", "76" },
+            { "ca", "82", "c9", "7d", "fa", "59", "47", "f0", "ad", "d4", "a2", "af", "9c", "a4", "72", "c0" },
+            { "b7", "fd", "93", "26", "36", "3f", "f7", "cc", "34", "a5", "e5", "f1", "71", "d8", "31", "15" },
+            { "04", "c7", "23", "c3", "18", "96", "05", "9a", "07", "12", "80", "e2", "eb", "27", "b2", "75" },
+            { "09", "83", "2c", "1a", "1b", "6e", "5a", "a0", "52", "3b", "d6", "b3", "29", "e3", "2f", "84" },
+            { "53", "d1", "00", "ed", "20", "fc", "b1", "5b", "6a", "cd", "be", "39", "4a", "4c", "58", "cf" },
+            { "d0", "ef", "aa", "fb", "43", "4d", "33", "85", "45", "f9", "02", "7f", "50", "3c", "9f", "a8" },
+            { "51", "a3", "40", "87", "92", "9d", "38", "f5", "bc", "b6", "da", "21", "10", "ff", "f3", "d2" },
+            { "cd", "0c", "13", "ec", "5f", "97", "44", "17", "c4", "a7", "7e", "3d", "64", "5d", "19", "73" },
+            { "60", "81", "4f", "dc", "22", "2a", "90", "88", "46", "ee", "b8", "14", "de", "5e", "0b", "db" },
+            { "e0", "32", "3a", "0a", "49", "06", "24", "5c", "c2", "d3", "ac", "62", "91", "95", "e4", "79" },
+            { "e7", "c8", "37", "6d", "8d", "d5", "4e", "a9", "6c", "56", "f4", "ea", "65", "7a", "ae", "08" },
+            { "ba", "78", "25", "2e", "1c", "a6", "b4", "c6", "e8", "dd", "74", "lf", "4b", "bd", "8b", "8a" },
+            { "70", "3e", "b5", "66", "48", "03", "f6", "0e", "61", "35", "57", "b9", "86", "c1", "1d", "9e" },
+            { "e1", "f8", "98", "11", "69", "d9", "8e", "94", "9b", "le", "87", "e9", "ce", "55", "28", "7e" },
+            { "8c", "a1", "89", "0d", "bf", "e6", "42", "68", "41", "99", "2d", "0f", "b0", "54", "bb", "16" }, };
+
+    final private String[][] invSbox = new String[][] {
+            { "52", "09", "6a", "d5", "30", "36", "a5", "38", "bf", "40", "a3", "9e", "81", "f3", "d7", "fb" },
+            { "7c", "e3", "39", "82", "9b", "2f", "ff", "87", "34", "8e", "43", "44", "c4", "de", "e9", "cb" },
+            { "54", "7b", "94", "32", "a6", "c2", "23", "3d", "ee", "4c", "95", "0b", "42", "fa", "c3", "4e" },
+            { "08", "2e", "a1", "66", "28", "d9", "24", "b2", "76", "5b", "a2", "49", "6d", "8b", "d1", "25" },
+            { "72", "f8", "f6", "64", "86", "68", "98", "16", "d4", "a4", "5c", "cc", "5d", "65", "b6", "92" },
+            { "6c", "70", "48", "50", "fd", "ed", "b9", "da", "5e", "15", "46", "57", "a7", "8d", "9d", "84" },
+            { "90", "d8", "ab", "00", "8c", "bc", "d3", "0a", "f7", "e4", "58", "05", "b8", "b3", "45", "06" },
+            { "d0", "2c", "le", "8f", "ca", "3f", "0f", "02", "c1", "af", "bd", "03", "01", "13", "8a", "6b" },
+            { "3a", "91", "11", "41", "4f", "67", "dc", "ea", "97", "f2", "cf", "ce", "f0", "b4", "e6", "73" },
+            { "96", "ac", "74", "22", "e7", "ad", "35", "85", "e2", "f9", "37", "e8", "1c", "75", "df", "6e" },
+            { "47", "f1", "la", "71", "1d", "29", "c5", "89", "6f", "b7", "62", "0e", "aa", "18", "be", "1b" },
+            { "fc", "56", "3e", "4b", "c6", "d2", "79", "20", "9a", "db", "c0", "fe", "78", "cd", "5a", "f4" },
+            { "1f", "dd", "a8", "33", "88", "07", "c7", "31", "b1", "12", "10", "59", "27", "80", "ec", "5f" },
+            { "60", "51", "7f", "a9", "19", "b5", "4a", "0d", "2d", "e5", "7a", "9f", "93", "c9", "9c", "ef" },
+            { "a0", "e0", "eb", "4d", "ae", "2a", "f5", "b0", "c8", "eb", "bb", "3c", "83", "53", "99", "61" },
+            { "17", "2b", "04", "7e", "ba", "77", "d6", "26", "e1", "69", "14", "63", "55", "21", "0c", "7d" } };
+
+    public char[] g(char[] word) {
+        /*
+         * RotWord performs a one-byte circular left shift on a word. This means that an
+         * input word [B0, B1, B2, B3] is transformed into [B1, B2, B3, B0].
+         */
+        char temp = word[0];
+        word[0] = word[1];
+        word[1] = word[2];
+        word[2] = temp;
+
+        // SubWord performs a byte substitution on each
+        // byte of its input word, using the S-box.
+
+        for (int count = 0; count < 4; count++) {
+
+            // firstly take the character from input stream
+            char chr = word[count];
+            // then convert Character to hexadecimal string
+            String hexStr = Integer.toHexString(chr);
+            // now divide the hex string into two subpart 1hex value and 2hex value
+            // String divion in two half
+            // e.g. hexCode1 = "4"; hexCode2 = "d"
+            String hexCode1 = hexStr.substring(0, 1); // give me first 4bits into hex code from string
+            String hexCode2 = hexStr.substring(1, 2); // give me first 4bits into hex code from string
+
+            // now get the integer index from hexcode to find the x and y coordinate to
+            // subsitute
+            int rowSbIdx = Integer.valueOf(hexCode1, 16).intValue();
+            int colSbIdx = Integer.valueOf(hexCode2, 16).intValue();
+            // convert hexCode to character then store to given grid.
+            word[count] = (char) Integer.parseInt(this.sBox[rowSbIdx][colSbIdx], 16);
+        }
+
+        /*
+         * The round constant is a word in which the three rightmost bytes are always 0.
+         * Thus, the effect of an XOR of a word with Rcon is to only perform an XOR on
+         * the left- most byte of the word. The round constant is different for each
+         * round and is defined as Rcon[j] = (RC[j], 0, 0, 0),
+         */
+
+        String RC[] = { "01", "02", "04", "08", "08", "10", "20", "40", "80", "1B", "36" };
+
+        char roundKey = (char) Integer.parseInt(RC[this.round], 16);
+        char[] rcList = { roundKey, '0', '0', '0' }; // making the round constant array for Xor
+
+        // Do Exor of the cipherWord with the rcList
+        for (int idx = 0; idx < 4; idx++)
+            word[idx] = (char) (word[idx] ^ rcList[idx]);
+
+        return word;
+    }
+
     public char[][] keyExpand(char[][] stateKey) {
+
         // genertate the keys state
-        char[] genkeyWordState; // = g(stateKey);
-        char[][] genState;
+        char[] genkeyWordState = new char[4];
+        // storing the word3 for pass to g() function
         for (int r = 0; r < 4; r++)
-            genState[r][0] ^= genkeyWordState[r];
+            genkeyWordState[r] = stateKey[r][3];
+
+        // calling g() function
+        genkeyWordState = g(genkeyWordState);
+        char[][] genState = new char[4][4];
+        // exoring ^ with the given stateKey word0 with generated work from g() function
+
+        for (int r = 0; r < 4; r++)
+            genState[r][0] = (char) (stateKey[r][0] ^ genkeyWordState[r]);
 
         for (int r = 0; r < 4; r++) {
             for (int c = 1; c < 4; c++) {
-                genState[r][c] = genState[r][c - 1] ^ stateKey[r][c];
+                genState[r][c] = (char) (genState[r][c - 1] ^ stateKey[r][c]);
             }
         }
         return genState;
     }
 
     public char[][] substitute(char[][] state) {
-        final String[][] sBox = new String[][] {
-                { "63", "7c", "77", "7b", "f2", "6b", "6f", "c5", "30", "01", "67", "2b", "fe", "d7", "ab", "76" },
-                { "ca", "82", "c9", "7d", "fa", "59", "47", "f0", "ad", "d4", "a2", "af", "9c", "a4", "72", "c0" },
-                { "b7", "fd", "93", "26", "36", "3f", "f7", "cc", "34", "a5", "e5", "f1", "71", "d8", "31", "15" },
-                { "04", "c7", "23", "c3", "18", "96", "05", "9a", "07", "12", "80", "e2", "eb", "27", "b2", "75" },
-                { "09", "83", "2c", "1a", "1b", "6e", "5a", "a0", "52", "3b", "d6", "b3", "29", "e3", "2f", "84" },
-                { "53", "d1", "00", "ed", "20", "fc", "b1", "5b", "6a", "cd", "be", "39", "4a", "4c", "58", "cf" },
-                { "d0", "ef", "aa", "fb", "43", "4d", "33", "85", "45", "f9", "02", "7f", "50", "3c", "9f", "a8" },
-                { "51", "a3", "40", "87", "92", "9d", "38", "f5", "bc", "b6", "da", "21", "10", "ff", "f3", "d2" },
-                { "cd", "0c", "13", "ec", "5f", "97", "44", "17", "c4", "a7", "7e", "3d", "64", "5d", "19", "73" },
-                { "60", "81", "4f", "dc", "22", "2a", "90", "88", "46", "ee", "b8", "14", "de", "5e", "0b", "db" },
-                { "e0", "32", "3a", "0a", "49", "06", "24", "5c", "c2", "d3", "ac", "62", "91", "95", "e4", "79" },
-                { "e7", "c8", "37", "6d", "8d", "d5", "4e", "a9", "6c", "56", "f4", "ea", "65", "7a", "ae", "08" },
-                { "ba", "78", "25", "2e", "1c", "a6", "b4", "c6", "e8", "dd", "74", "lf", "4b", "bd", "8b", "8a" },
-                { "70", "3e", "b5", "66", "48", "03", "f6", "0e", "61", "35", "57", "b9", "86", "c1", "1d", "9e" },
-                { "e1", "f8", "98", "11", "69", "d9", "8e", "94", "9b", "le", "87", "e9", "ce", "55", "28", "7e" },
-                { "8c", "a1", "89", "0d", "bf", "e6", "42", "68", "41", "99", "2d", "0f", "b0", "54", "bb", "16" }, };
 
         // now we have to do Operation of substitute byte
         for (int r = 0; r < 4; r++) {
@@ -53,7 +134,7 @@ class AES {
                 int rowSbIdx = Integer.valueOf(hexCode1, 16).intValue();
                 int colSbIdx = Integer.valueOf(hexCode2, 16).intValue();
                 // convert hexCode to character then store to given grid.
-                state[r][c] = (char) Integer.parseInt(sBox[rowSbIdx][colSbIdx], 16);
+                state[r][c] = (char) Integer.parseInt(this.sBox[rowSbIdx][colSbIdx], 16);
             }
         }
         return state;
@@ -78,7 +159,7 @@ class AES {
 
     public char[][] mixCol(char[][] state) {
         // declare of matrix which finite multiplication in nature
-
+        return state;
     }
 
     public char[][] invShiftRows(char[][] state) {
@@ -100,23 +181,6 @@ class AES {
     }
 
     public char[][] invSubstitute(char[][] state) {
-        final String[][] invSbox = new String[][] {
-                { "52", "09", "6a", "d5", "30", "36", "a5", "38", "bf", "40", "a3", "9e", "81", "f3", "d7", "fb" },
-                { "7c", "e3", "39", "82", "9b", "2f", "ff", "87", "34", "8e", "43", "44", "c4", "de", "e9", "cb" },
-                { "54", "7b", "94", "32", "a6", "c2", "23", "3d", "ee", "4c", "95", "0b", "42", "fa", "c3", "4e" },
-                { "08", "2e", "a1", "66", "28", "d9", "24", "b2", "76", "5b", "a2", "49", "6d", "8b", "d1", "25" },
-                { "72", "f8", "f6", "64", "86", "68", "98", "16", "d4", "a4", "5c", "cc", "5d", "65", "b6", "92" },
-                { "6c", "70", "48", "50", "fd", "ed", "b9", "da", "5e", "15", "46", "57", "a7", "8d", "9d", "84" },
-                { "90", "d8", "ab", "00", "8c", "bc", "d3", "0a", "f7", "e4", "58", "05", "b8", "b3", "45", "06" },
-                { "d0", "2c", "le", "8f", "ca", "3f", "0f", "02", "c1", "af", "bd", "03", "01", "13", "8a", "6b" },
-                { "3a", "91", "11", "41", "4f", "67", "dc", "ea", "97", "f2", "cf", "ce", "f0", "b4", "e6", "73" },
-                { "96", "ac", "74", "22", "e7", "ad", "35", "85", "e2", "f9", "37", "e8", "1c", "75", "df", "6e" },
-                { "47", "f1", "la", "71", "1d", "29", "c5", "89", "6f", "b7", "62", "0e", "aa", "18", "be", "1b" },
-                { "fc", "56", "3e", "4b", "c6", "d2", "79", "20", "9a", "db", "c0", "fe", "78", "cd", "5a", "f4" },
-                { "1f", "dd", "a8", "33", "88", "07", "c7", "31", "b1", "12", "10", "59", "27", "80", "ec", "5f" },
-                { "60", "51", "7f", "a9", "19", "b5", "4a", "0d", "2d", "e5", "7a", "9f", "93", "c9", "9c", "ef" },
-                { "a0", "e0", "eb", "4d", "ae", "2a", "f5", "b0", "c8", "eb", "bb", "3c", "83", "53", "99", "61" },
-                { "17", "2b", "04", "7e", "ba", "77", "d6", "26", "e1", "69", "14", "63", "55", "21", "0c", "7d" } };
 
         // now we have to do Operation of substitute byte
         for (int r = 0; r < 4; r++) {
@@ -136,7 +200,7 @@ class AES {
                 int rowSbIdx = Integer.valueOf(hexCode1, 16).intValue();
                 int colSbIdx = Integer.valueOf(hexCode2, 16).intValue();
                 // convert hexCode to character then store to given grid.
-                state[r][c] = (char) Integer.parseInt(invSbox[rowSbIdx][colSbIdx], 16);
+                state[r][c] = (char) Integer.parseInt(this.invSbox[rowSbIdx][colSbIdx], 16);
             }
         }
         return state;
@@ -174,39 +238,33 @@ public class cyperAES {
         }
 
         AES cyperGen = new AES();
-
-        char subtiGrid[][] = cyperGen.substitute(plainTxtGrid);
-        System.out.println("Subsitute");
-        for (int r = 0; r < 4; r++) {
-            for (int c = 0; c < 4; c++) {
-                System.out.print(plainTxtGrid[r][c] + " ");
-            }
-            System.out.println("");
-        }
-
-        char shiftGrid[][] = cyperGen.shiftRows(plainTxtGrid);
-        System.out.println("shift grid");
-        for (int r = 0; r < 4; r++) {
-            for (int c = 0; c < 4; c++) {
-                System.out.print(shiftGrid[r][c] + " ");
-            }
-            System.out.println("");
-        }
-
-        char invShiftGrid[][] = cyperGen.invShiftRows(shiftGrid);
-        System.out.println("Inv shift grid");
-        for (int r = 0; r < 4; r++) {
-            for (int c = 0; c < 4; c++) {
-                System.out.print(invShiftGrid[r][c] + " ");
-            }
-            System.out.println("");
-        }
-
-        char invSubtiGrid[][] = cyperGen.invSubstitute(subtiGrid);
-        System.out.println(" inv Subsitute");
-        for (int r = 0; r < 4; r++) {
-            for (int c = 0; c < 4; c++) {
-                System.out.print(invSubtiGrid[r][c] + " ");
+        /*
+         * char subtiGrid[][] = cyperGen.substitute(plainTxtGrid);
+         * System.out.println("Subsitute"); for (int r = 0; r < 4; r++) { for (int c =
+         * 0; c < 4; c++) { System.out.print(plainTxtGrid[r][c] + " "); }
+         * System.out.println(""); }
+         * 
+         * char shiftGrid[][] = cyperGen.shiftRows(plainTxtGrid);
+         * System.out.println("shift grid"); for (int r = 0; r < 4; r++) { for (int c =
+         * 0; c < 4; c++) { System.out.print(shiftGrid[r][c] + " "); }
+         * System.out.println(""); }
+         * 
+         * char invShiftGrid[][] = cyperGen.invShiftRows(shiftGrid);
+         * System.out.println("Inv shift grid"); for (int r = 0; r < 4; r++) { for (int
+         * c = 0; c < 4; c++) { System.out.print(invShiftGrid[r][c] + " "); }
+         * System.out.println(""); }
+         * 
+         * char invSubtiGrid[][] = cyperGen.invSubstitute(subtiGrid);
+         * System.out.println(" inv Subsitute"); for (int r = 0; r < 4; r++) { for (int
+         * c = 0; c < 4; c++) { System.out.print(invSubtiGrid[r][c] + " "); }
+         * System.out.println(""); }
+         */
+        char[][] key = { { '3', 'f', 'r', 't' }, { 'd', 'c', 'c', 'w' }, { 'r', 'c', '6', '2' },
+                { '5', '7', 'b', '@' } };
+        char[][] keysExp = cyperGen.keyExpand(key);
+        for (int r = 0; r < 2; r++) {
+            for (int c = 0; c < 2; c++) {
+                System.out.print(keysExp[r][c] + " ");
             }
             System.out.println("");
         }
